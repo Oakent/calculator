fdaa3c52e5f06b10aa5c9ed8f0d680ace1b5ad6f
Oakent <oakentravis@gmail.com>
2023-05-30 19:08:19 +0100

    add basic math functions

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-add = (num1, num2) => {};
+add = (num1, num2) => {
+  return num1 + num2;
+};
+subtract = (num1, num2) => {
+  return num1 - num2;
+};
+multiply = (num1, num2) => {
+  return num1 * num2;
+};
+divide = (num1, num2) => {
+  return num1 / num2;
+};
