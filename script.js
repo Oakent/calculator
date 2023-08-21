34f02b99d336f9430dac4eb73cedcd2ee645f53d
oakent <oakentravis@gmail.com>
2023-08-21 16:02:13 -0500

    add divide javascript

--- script.js
+++ script.js
@@ -13,7 +13,10 @@ divide = (num1, num2) => {
 
 let display = document.getElementById("display");
 let seven = document.getElementById("7");
+let divide = document.getElementById("divide");
 
 seven.addEventListener("click", () => {
   display.innerHTML = display.innerHTML += "7";
 });
+
+// variable
