3279e3bdbe33b8bc60c85884a16b656bbbb5a9d5
oakent <oakentravis@gmail.com>
2023-08-21 11:56:04 -0500

    add event listener

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ multiply = (num1, num2) => {
 divide = (num1, num2) => {
   return num1 / num2;
 };
+
+let display = document.getElementById("display");
+let seven = document.getElementById("7");
+
+seven.addEventListener("click", () => {
+  display.innerHTML = display.innerHTML += "7";
+});
