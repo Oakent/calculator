99e351a1af0cb4b0bca8af9b3f85b20e2507b1bd
oakent <oakentravis@gmail.com>
2023-08-30 16:42:04 -0500

    add operate function

--- script.js
+++ script.js
@@ -11,12 +11,39 @@ divide = (num1, num2) => {
   return num1 / num2;
 };
 
-let display = document.getElementById("display");
-let seven = document.getElementById("7");
-let divide = document.getElementById("divide");
-
+operate = (operand, num1, num2) => {
+  let result = "";
+  num1 = Number(num1);
+  num2 = Number(num2);
+  if (operand == "+") {
+    result = add(num1, num2);
+    console.log({ result });
+  }
+  display.innerHTML = result;
+};
+let num1;
+let num2;
+const display = document.getElementById("display");
+const seven = document.getElementById("seven");
+const btnDivide = document.getElementById("divide");
+const btnAdd = document.getElementById("add");
+const btnEquals = document.getElementById("equals");
+display.innerHTML = 0;
+let operand = "";
 seven.addEventListener("click", () => {
-  display.innerHTML = display.innerHTML += "7";
+  if (display.innerHTML == "+") {
+    display.innerHTML = 0;
+  }
+  display.innerHTML = display.innerHTML += 7;
 });
 
-// variable
+btnEquals.addEventListener("click", () => {
+  num2 = display.innerHTML;
+  operate(operand, num1, num2);
+});
+
+btnAdd.addEventListener("click", () => {
+  num1 = display.innerHTML;
+  operand = "+";
+  display.innerHTML = "+";
+});
